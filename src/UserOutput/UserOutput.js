6839a2c560db758f65872e3806343b3fe28d0c10
pchandra1234 <pradeepchittipch@example.com>
2020-01-29 00:36:22 -0600

    first commit related to assignment 1

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const userOutput = (props) => {
+    return (
+        <div className="UserOutput">
+            <p>Username: {props.userName}</p>
+            <p>I hope I'll be overwritten!</p>
+        </div>
+    );
+};
+
+export default userOutput;
